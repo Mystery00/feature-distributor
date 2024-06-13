6a5cceb1eac680b867aa805c934d8efc4d8a1824
Mystery0 <mystery0dyl520@gmail.com>
2024-06-14 00:06:28 +0800

    fix: 接口数据调整

--- endpoint/web/project/list.go
+++ endpoint/web/project/list.go
@@ -1,10 +1,21 @@
 package project
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 var list gin.HandlerFunc = func(context *gin.Context) {
 	context.JSON(200, gin.H{
 		"message": "ok",
-		"data":    []string{"project1", "project2"},
+		"data": []gin.H{
+			{
+				"id":   1,
+				"name": "Project 1",
+			},
+			{
+				"id":   2,
+				"name": "Project 2",
+			},
+		},
 	})
 }
